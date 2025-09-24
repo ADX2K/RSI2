bf7563e3edf9f87cfc1cd4e734c23cf8a9da798d
Mohamed Adel Aiouaz <43122353+ADX2K@users.noreply.github.com>
2025-09-24 16:36:47 +0100

    Delete S2/Projet d'integration directory

--- S2/Projet d'integration/project-integration/website/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from accounts import views
-
-urlpatterns = [
-    path("", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("dashboard/", views.admin_dashboard, name="dashboard"),
-]
